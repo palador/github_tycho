2eccb7f2d9e617cde489b84fe2dd9cffaf5823ed
palador <palador@projektangriff.com>
2012-02-27 23:13:58 +0100

    fixed product-file

--- application/src/org/angriff/githubtycho/application/MyApplication.java
+++ application/src/org/angriff/githubtycho/application/MyApplication.java
@@ -2,15 +2,26 @@ package org.angriff.githubtycho.application;
 
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
 public class MyApplication implements IApplication {
 
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
 		System.out.println("start app");
-		
-		Thread.sleep(2000);
-		
+
+		Display firstDisplay = new Display();
+		Shell firstShell = new Shell(firstDisplay);
+		firstShell.setText("First Example");
+		firstShell.setSize(200, 100);
+		firstShell.open();
+		while (!firstShell.isDisposed()) {
+			if (!firstDisplay.readAndDispatch())
+				firstDisplay.sleep();
+		}
+		firstDisplay.dispose();
+
 		return IApplication.EXIT_OK;
 	}
 
